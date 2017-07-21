0d92ae1c3eec5a0af76b18b572d123647eeea9b8
wguo <wguo25@its.jnj.com>
2017-07-21 15:27:54 -0400

    Version 1.0-3
    added more examples

--- R/sl.sim.R
+++ R/sl.sim.R
@@ -4,8 +4,9 @@
 #' @param file The name of the file which the data are to be read from. See details in \code{\link{read.table}}.
 #' @param header A logical value indicating whether the file contains the names of the variables as its first line. Default is \code{FALSE}. See details in \code{\link{read.table}}.
 #' @param sep The field separator character. Default is \code{","}. See details in \code{\link{read.table}}.
+#' @param ... arguments to be passed to \code{\link{read.table}} methods.
 #' @details In each line of the input file, the parameters must be ordered in accordance as follows: \code{pt}, \code{start.level}, \code{nsim}, \code{truep}. See details in \code{\link{read.table}}.
-#' @return The functions summary is used to obtain and print a summary table of the results. An object of class \code{"dec.sim"} (1 scenario) or \code{"sl.sim"} (more than 1 scenarios)is a list containing:
+#' @return The function \code{\link{summary}} is used to obtain and print a summary table of the results. An object of class \code{"dec.sim"} (1 scenario) or \code{"sl.sim"} (more than 1 scenarios)is a list containing:
 #'  \item{MTD}{A vector of dose levels giving the recommended maximum tolerated dose (MTD) at the end of the trial.}
 #'  \item{n.patients}{The average number of patients dosed at each level.}
 #   \item{alpha.one}{a vector of true type 1 error for right-tailed test.}
@@ -19,10 +20,14 @@
 #' @examples
 #' dt <- dec.table(0.6,0.4,0.2,0.3,0.3,c(3,3,3))
 #' test.file <- system.file("extdata", "testS.csv", package = "tsdf")
-#' out <- sl.sim(dt$table, test.file)
+#' # use a customized decision table
+#' table.file <- system.file("extdata", "decTable.csv", package = "tsdf")
+#' dec <- read.table(table.file, sep=",", col.names=c(3,4,8,10), row.names = 1, check.names = FALSE)
+#' out1 <- sl.sim(dt$table, test.file)
+#' out2 <- sl.sim(dec, test.file)
 
-sl.sim <- function(decTable, file, header = FALSE, sep = ",") {
-  sl <- read.table(file, header, sep)
+sl.sim <- function(decTable, file, header = TRUE, sep = ",", ...) {
+  sl <- read.table(file, header, sep, ...)
   out <- lapply(1:nrow(sl), function(i){
     info <- sl[i, ][!is.na(sl[i, ])]
     n.info <- length(info)
